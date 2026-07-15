1e3c3179b5fb1235523e68ac46aecc4d0e4eac54
Emriqurrizal Yahya Nurramdahan <emriqurrizal@gmail.com>
2026-07-15 20:41:28 +0700

    chore: fix hr issues

--- dbt_strava/models/staging/stg_heartrate_zones.sql
+++ dbt_strava/models/staging/stg_heartrate_zones.sql
@@ -4,15 +4,24 @@ with streams as (
     select * from {{ source('bronze', 'raw_streams') }}
     where heartrate is not null
 ),
+deltas as (
+    select
+        strava_id,
+        heartrate,
+        time_offset - lag(time_offset, 1) over (
+            partition by strava_id order by time_offset
+        ) as seconds_elapsed
+    from streams
+),
 zones as (
     select * from {{ ref('hr_zones') }}
 )
 
 select
-    s.strava_id,
+    d.strava_id,
     z.zone,
-    -- assuming 1 second intervals for streams
-    count(*) as seconds_in_zone
-from streams s
-join zones z on s.heartrate >= z.min_hr and s.heartrate <= z.max_hr
+    sum(d.seconds_elapsed) as seconds_in_zone
+from deltas d
+join zones z on d.heartrate >= z.min_hr and d.heartrate <= z.max_hr
+where d.seconds_elapsed is not null
 group by 1, 2
