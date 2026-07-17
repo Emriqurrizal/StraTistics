7245ca46a03399202c690cba20e3727086797656
Emriqurrizal Yahya Nurramdahan <emriqurrizal@gmail.com>
2026-07-17 18:11:28 +0700

    fixed gear mileage bug

--- dbt_strava/models/marts/dim_gear.sql
+++ dbt_strava/models/marts/dim_gear.sql
@@ -2,13 +2,24 @@
 
 with raw_gear as (
     select * from {{ source('bronze', 'raw_gear') }}
+),
+valid_activities as (
+    select * from {{ ref('stg_activities') }}
+),
+gear_mileage as (
+    select 
+        gear_id,
+        sum(distance_km) as valid_distance_km
+    from valid_activities
+    group by 1
 )
 
 select
-    gear_id,
-    name,
-    brand_name,
-    model_name,
-    distance / 1000.0 as cumulative_distance_km,
-    retired
-from raw_gear
+    r.gear_id,
+    r.name,
+    r.brand_name,
+    r.model_name,
+    coalesce(m.valid_distance_km, 0) as cumulative_distance_km,
+    r.retired
+from raw_gear r
+left join gear_mileage m on r.gear_id = m.gear_id
